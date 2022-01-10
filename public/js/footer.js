2609b94be653f46d4f4e8b9ae4bdab20979a24e2
Aashirurrehman <aashir.ur22@gmail.com>
2022-01-11 04:44:20 +0500

    6th comit
    All pages done except cart page and responsiveness

--- public/js/footer.js
+++ public/js/footer.js
@@ -2,65 +2,26 @@ const createFooter = () => {
   let footer = document.querySelector("footer");
 
   footer.innerHTML = `
-        <div class="footer-content">
-            <img src="../images/logo.png" class="logo" alt="">
-            <div class="footer-ul-container">
-                <ul class="category">
-                    <li class="category-tittle">
-                        Information
-                    </li>
-                    <li><a href="#" class="footer-link">Made to Measure</a></li>
-                    <li><a href="#" class="footer-link">Privacy Policy</a></li>
-                    <li><a href="#" class="footer-link">Terms & Conditions</a></li>
-                    <li><a href="#" class="footer-link">Exchange Policy(online Purchase)</a></li>
+  <section class="footer">
+
+    <h4>About Us</h4>
+    <p>About us pages serve much the same purpose for retailers who sell less-common or unique items as well. <br>
+        Additionally, they allow for an in-depth explanation of what brought the ecommerce store owner to the
+        specific market </p>
+
+    <div class="icons">
+        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+        <a href="#" class="google"><i class="fa fa-google"></i></a>
+        <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+        <a href="#" class="youtube"><i class="fa fa-youtube"></i></a>
+
+    </div>
 
 
-                </ul>
-                <ul class="category">
-                    <li class="category-tittle">
-                    Men Suiting
-                    </li>
-                    <li><a href="#" class="footer-link">Bespoke Suits</a></li>
-                    <li><a href="#" class="footer-link">Wedding Suits</a></li>
-                    <li><a href="#" class="footer-link">2 Piece Suits</a></li>
-                    <li><a href="#" class="footer-link">3 Piece Suits</a></li>
-                </ul>
-            </div>
 
-        </div>
-        <p class="footer-title">about company</p>
-        <p class="info">
-            The construction of our suits has to be executed with precision, because each piece is an
-            exquisite one of a kind work of art that our customers get to wear.”
+    </section>
 
-            Aashir-Outfitters started with the dream of helping make bespoke suiting of the highest quality accessible
-            to
-            a wider market – unlike any brand ever before. We imagined a new era of completely customized suiting
-            with the most luxurious fabrics, but in a way that allowed more people to buy & wear our exquisite,
-            tailored suits.
-
-            Aashir-Outfitters is our take on the traditional bespoke suit; steeped in Italian traditions of
-            craftsmanship
-            and creativity, as exhibited by the opulent couture houses of Milan. Such craftsmanship has always been
-            reminiscent of a world that is exclusively available to the ultra-wealthy. Thus, we at Aashir-Outfitters
-            wanted to take this idea of creating unique pieces conceived by couturiers with just one client in mind.
-            In this way, we were able to modify this traditional idea of bespoke suiting & merge it into a more
-            accessible business model, so that more and more people can enjoy the luxury of owning a completely one
-            of a kind, exquisitely designed, bespoke suit. This is what we at Aashir-Outfitters refer to as the modern
-            day renaissance in traditional couture.
-        </p>
-        <p class="info">Support emails - help@aof.com,
-            customersupport@aof.com
-        </p>
-        <p class="info">Contact-us: 111-263-263, 111-263-264</p>
-
-        <div class="footer-social-container">
-            <div>
-                <a href="#" class="social-links">Instagram</a>
-                <a href="#" class="social-links">Facebook</a>
-                <a href="#" class="social-links">Twitter</a>
-            </div>
-        </div>
         <p class="footer-credits">Aashir-Outfitters, Best suiting online store</p>
 
     `;
--- public/js/nav.js
+++ public/js/nav.js
@@ -6,26 +6,26 @@ const createNav = () => {
     nav.innerHTML = `
 
         <div class="nav">
-            <img src="../images/logo.png"  class="brand-logo" alt="logo">
+            <a href="home.html"><img src="../images/logo.png"  class="brand-logo" alt="logo"></a>
             <div class="nav-item">
                 <div class="search">
                     <input type="text" placeholder="search" class="search-box">
-                    <button class="search-btn">Search</button>
+                    <button class="search-btn"><a href="search.html" >Search</a></button>
                 </div>
-                <a href="#"><img src="../images/user.png" alt=""> </a>
-                <a href="#"><img src="../images/cart.png" alt=""> </a>
+                <a href="signin.html"><img src="../images/user.png" alt=""> </a>
+                <a href="cart.html"><img src="../images/cart.png" alt=""> </a>
             </div>
 
 
         </div>
         <i class="fa fa-close" onclick="hideMenu()"></i>
         <ul class="Link-container">
-            <li class="link-item"><a href="/public/home.html" class="Link">Home</a></li>
-            <li class="link-item"><a href="../About.html" class="Link">About Us</a></li>
-            <li class="link-item"><a href="../Bespoke.html" class="Link">Bespoke Suiting</a></li>
-            <li class="link-item"><a href="../3PieceSuit.html" class="Link">3 Piece Suits</a></li>
-            <li class="link-item"><a href="../wedding.html" class="Link">Wedding Suits</a></li>
-            <li class="link-item"><a href="../2PieceSuit.html" class="Link">2 Piece Suits</a></li>   
+            <li class="link-item"><a href="home.html" class="Link">Home</a></li>
+            <li class="link-item"><a href="About.html" class="Link">About Us</a></li>
+            <li class="link-item"><a href="Bespoke.html" class="Link">Bespoke Suiting</a></li>
+            <li class="link-item"><a href="3PieceSuit.html" class="Link">3 Piece Suits</a></li>
+            <li class="link-item"><a href="wedding.html" class="Link">Wedding Suits</a></li>
+            <li class="link-item"><a href="2PieceSuit.html" class="Link">2 Piece Suits</a></li>   
         </ul>
         <i class="fa fa-bars" onclick="showMenu()"></i>
     
